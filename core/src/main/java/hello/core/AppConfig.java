839fdf6ee36cdd803197595197dd93b1433424a0
taehyeok <ajaj3122@naver.com>
2022-12-13 17:37:18 +0900

    Refactor: Appconfig에서 역할과 구현을 한 눈에 파악

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolish;
 import hello.core.discount.FixDiscountPolish;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -9,12 +11,20 @@ import hello.core.order.OrderServiceImpl;
 
 // 애플리케이션 전체를 설정하고 구성함
 public class AppConfig {
-
+    // 역할을 다 파악할 수 있으며, private의 메서드를 호출하기 때문에 구현또한 AppConfig에서 파악할 수 있다.
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolish());
+        return new OrderServiceImpl(memberRepository(), discountPolish());
+    }
+
+    private DiscountPolish discountPolish() {
+        return new FixDiscountPolish();
     }
 }
