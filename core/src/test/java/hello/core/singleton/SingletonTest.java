46e024e4acc4cab307c60f650c9380a06314ac07
taehyeok <ajaj3122@naver.com>
2022-12-15 13:52:23 +0900

    Test: Spring Singleton Pattern - getBean을 사용하면 같은 객체를 받아옴

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +19,6 @@ public class SingletonTest {
         AppConfig appConfig = new AppConfig();
         // 1. 조회: 호출할 때 마다 객체를 생성
         MemberService memberService1 = appConfig.memberService();
-        // 1. 조회: 호출할 때 마다 객체를 생성
         MemberService memberService2 = appConfig.memberService();
         
         // 참조값이 다른 것을 확인 => 메모리가 계속 생성되어서 올라감(낭비)
@@ -41,5 +42,22 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
         // same : ==
         // equls : String이 같은지
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    public void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = applicationContext.getBean("memberService", MemberService.class);
+        MemberService memberService2 = applicationContext.getBean("memberService", MemberService.class);
+
+        // 참조값이 다른 것을 확인 => 메모리가 계속 생성되어서 올라감(낭비)
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
