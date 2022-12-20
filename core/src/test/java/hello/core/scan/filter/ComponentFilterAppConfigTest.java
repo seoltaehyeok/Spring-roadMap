9c9c66265499d88af423f704eb2cb06e2f5a81f3
taehyeok <ajaj3122@naver.com>
2022-12-20 15:05:58 +0900

    Fix: Test 코드 import 수정

--- core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
+++ core/src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -1,9 +1,7 @@
 package hello.core.scan.filter;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
