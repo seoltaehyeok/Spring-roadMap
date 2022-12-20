d1c0b209975a2ddba103ddd471a71cead4d61d75
taehyeok <ajaj3122@naver.com>
2022-12-20 13:13:49 +0900

    Test: 수동 빈 등록과 자동 빈 등록간의 중복 충돌 시 결과 테스트

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -14,5 +17,12 @@ import org.springframework.context.annotation.FilterType;
 )
 
 public class AutoAppConfig {
-
+    // Component를 통해 bean등록을 해주는 것과 수동으로 Bean 등록을 해주는 상황에서 Bean name을 동일하게 가져가면?
+    // => Overriding bean definition for bean 'memoryMemberRepository' (수동 빈이 자동 빈 등록을 오버라이딩하여 우선권을 가져서 정상 실행)
+    // => SpringBootApplication에서 실행하게 되면 수동 빈과 자동 빈 충돌 시 오류를 발생해준다.
+    // => The bean 'memoryMemberRepository', defined in class path resource A bean with that name has already been defined in file
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
--- core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+@Component  // Component를 기본으로 Bean 등록하게 해주면 MemoryMemberRepository는 memoryMemberRepository로 빈 등록이 된다.
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
