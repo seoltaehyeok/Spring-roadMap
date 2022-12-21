fd6b63091aa2b0672cd752a8208abb4b464eb0d1
taehyeok <ajaj3122@naver.com>
2022-12-21 13:36:58 +0900

    Feat: @Quilifier를 직접 어노테이션으로 만들어서 사용하기

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -7,7 +8,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@MainDiscountPolicy // 위 경우처럼 어노테이션을 만들어서 하는 것이 좋다.
+//@Qualifier("mainDiscountPolicy") // 이 경우는 텍스트기 때문에 컴파일 단계에서 오류를 찾지 못함
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
